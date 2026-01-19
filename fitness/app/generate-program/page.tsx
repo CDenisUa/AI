9ea329b22967859a3a30b46eb2497034221b9f94
Denys Chepiha <chepigadeveloper@gmail.com>
2026-01-19 08:14:54 +0100

    AI/fitness: Create project

---
+++ fitness/app/generate-program/page.tsx
@@ -0,0 +1,8 @@
+// Core
+import { FC } from 'react';
+
+const GenerateProgramPage: FC = () => {
+  return <div>GenerateProgram</div>;
+};
+
+export default GenerateProgramPage;
---
+++ fitness/app/profile/page.tsx
@@ -0,0 +1,8 @@
+// Core
+import { FC } from 'react';
+
+const ProfilePage: FC = () => {
+  return <div>ProfilePage</div>;
+};
+
+export default ProfilePage;
